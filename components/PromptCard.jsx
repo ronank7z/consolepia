4f233041b84e331acff0a985ff43a830b3a1e249
Ronan K <ronank.dev@gmail.com>
2024-03-20 21:09:55 +0700

    fix path for edit and delete

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -65,20 +65,21 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
 				#{post.tag}
 			</p>
 
-			{session?.user.id === post.creator._id && (
-				<div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
-					<p
-						className="font-inter text-sm green_gradient cursor-pointer"
-						onClick={handleEdit}>
-						Edit
-					</p>
-					<p
-						className="font-inter text-sm orange_gradient cursor-pointer"
-						onClick={handleDelete}>
-						Delete
-					</p>
-				</div>
-			)}
+			{session?.user.id === post.creator._id &&
+				pathName === `/profile/${session?.user.id}` && (
+					<div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
+						<p
+							className="font-inter text-sm green_gradient cursor-pointer"
+							onClick={handleEdit}>
+							Edit
+						</p>
+						<p
+							className="font-inter text-sm orange_gradient cursor-pointer"
+							onClick={handleDelete}>
+							Delete
+						</p>
+					</div>
+				)}
 		</div>
 	);
 };
