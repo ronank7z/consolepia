d85fb9b0b81742bf4bfcc2eda1fd9c74d730f716
Ronan K <ronank.dev@gmail.com>
2024-03-21 17:44:52 +0700

    google sign in fix remove match on User Schema

--- app/page.jsx
+++ app/page.jsx
@@ -6,11 +6,12 @@ const Home = () => {
 			<h1 className="head_text text-center">
 				Discover & Share
 				<br className="max-md:hidden" />
-				<span className="orange_gradient text-center"> AI-Powered Prompts</span>
+				<span className="orange_gradient text-center"> People Minds</span>
 			</h1>
 			<p className="desc text-center">
-				Consolepia is an open-source AI prompting tool for modern world to
-				discover, create, and share creative prompts
+				Consolepia is a platform where you can explore a world of ideas and
+				connect with like-minded individuals. Share your thoughts, spark
+				conversations, and be inspired by others.
 			</p>
 
 			<Feed />
